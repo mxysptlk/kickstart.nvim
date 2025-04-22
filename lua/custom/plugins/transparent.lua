47ed18f405a7ef7ed28b2a3637d38c9df1e77d07
William Smith <pastorofmuppets@me.com>
2025-04-22 09:58:17 -0700

    seperatad remaining plugins

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,5 +1,18 @@
 return {
   {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('onedark').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  {
     'xiyaowong/transparent.nvim',
     lazy = false,
     init = function()
